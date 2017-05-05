1edcbcdb3a006776c2e7cfa6810e72bb20e9d108
Junho Park <cnaa97@gmail.com>
2017-05-06 07:28:53 +0900

    remove log, edit title

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ $(function () {
     * smooth scroll
     */
     $('.category-item-link').click(function(){
-        console.log($.attr(this, 'href').substr(1));
         $('html, body').animate({
             scrollTop: $(`[name="${$.attr(this, 'href').substr(1)}"]`).offset().top - 16
         }, 500);
