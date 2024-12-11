2a2aa84e9d4b8010c3f6ef2dc3b95a6c55a4ce20
Junho Park <cnaa97@gmail.com>
2024-12-11 15:56:51 +0900

    fix fb comments

---
+++ src/components/FbComment.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+import { has } from "lodash/object";
+
+const FbComment = ({ pathname, className }: { pathname: string; className?: string }) => {
+  const fbRef = useRef(null);
+  
+  useEffect(() => {
+    if (globalThis.document && has(globalThis.window, "FB")) {
+      globalThis.FB.XFBML.parse(fbRef.current);
+    }
+  }, []);
+  
+  return (
+    <div ref={fbRef} className={className}>
+      <div
+        className="fb-comments"
+        data-href={`${globalThis.location.origin}${pathname}`}
+        data-width="100%"
+        data-numposts="10"
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default FbComment;
--- src/theme/BlogPostItem/Footer/index.js
+++ src/theme/BlogPostItem/Footer/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Footer from '@theme-original/BlogPostItem/Footer';
 import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
+import FbComment from '../../../components/FbComment';
 
 export default function FooterWrapper(props) {
-  const { isBlogPostPage } = useBlogPost();
+  const { isBlogPostPage, metadata, ...o } = useBlogPost();
   return (
     <>
-      {isBlogPostPage && <div className='mt-8'>
-        <div className="fb-comments" data-numposts="10" data-lazy="false" data-width="100%" />
-      </div>}
+      {isBlogPostPage && <FbComment pathname={metadata?.permalink} />}
       <Footer {...props} />
     </>
   );
